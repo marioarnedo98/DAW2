6d7f342704e3c153a2494a9631973e99a37e2397
Mario <marioarnedo1@gmail.com>
2018-09-24 13:02:19 +0200

    2409

--- Desarrollo Web Entorno Cliente/1Ev/Tema 2/Write a program 2/js/ejercicio9.js
+++ Desarrollo Web Entorno Cliente/1Ev/Tema 2/Write a program 2/js/ejercicio9.js
@@ -5,17 +5,33 @@ var arraycadena=[];
 var definitiva=[];
 for(x=fruta.length;x>=0;x--) {
     arraycadena.push(fruta.charAt(x));
-
 }  
-alert(arraycadena);
-definitiva = arraycadena.splice(0,1);
-alert(definitiva);
 switch (fruta) {
     case "Manzana":
-        alert("Holi");
+        alert(fruta+"\n"+arraycadena);
+        break;
+        case "Pera":
+        alert(fruta+"\n"+arraycadena);
+        break;
+        case "Fresa":
+        alert(fruta+"\n"+arraycadena);
+        break;
+        case "Nectarina":
+        alert(fruta+"\n"+arraycadena);
+        break;
+        case "Kiwi":
+        alert(fruta+"\n"+arraycadena);
+        break;
+        case "Mango":
+        alert(fruta+"\n"+arraycadena);
+        break;
+        case "Frambuesa":
+        alert(fruta+"\n"+arraycadena);
         break;
 
+
     default:
+        alert("No existe "+fruta);
         break;
     }
 }
