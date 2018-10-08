7989e5ac26a784d8c31ef897f82e0b13290ada3a
Mario <marioarnedo1@gmail.com>
2018-10-08 10:31:51 +0200

    Cosas de java

---
+++ Desarrollo Web Entorno Servidor/Programa-me/1/abuela.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+      Scanner nombrereal= new Scanner(System.in);
+      System.print.ln(nombrereal);
+    }
+
+}
---
+++ Desarrollo Web Entorno Servidor/Programa-me/programa-me/src/vieja.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class vieja {
+
+	public static void main(String[] args) {
+		String variableString;
+		   
+		  Scanner nombre_real=new Scanner(System.in);
+		  System.out.print("Ingrese un texto: ");
+		  System.out.println("Texto ingresado: "+nombre_real.next());
+		  
+	}
+
+}
