fe4ff83a5e33b38fbb4dc94637f9a753008e9e32
Mario <marioarnedo1@gmail.com>
2018-10-13 14:07:03 +0200

    1310

---
+++ Desarrollo Web Entorno Servidor/programa-me/src/vieja.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.lang.Object;
+import java.util.ArrayList;
+import java.util.*;
+
+public class vieja {
+	String nombreaBuscar;
+	String nietos;
+
+	public void separarNietos(String nombreaBuscar, String nietos) {
+		this.nombreaBuscar = nombreaBuscar;
+		this.nietos = nietos;
+		nietos.toLowerCase();
+		String partes[] = nietos.split(",");// separamos por comas
+		int j = partes.length - 1;// obtenemos el ultimo valor (partes[j])
+		if (partes.length >= 2) {
+			if (nombreaBuscar.equals(partes[j])) {
+				System.out.println("Coinciden");
+			} else {
+				System.out.println("No coincide");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		vieja t1 = new vieja();
+		t1.separarNietos("mario", "juan,pepe,maria,mario");
+	}
+}
