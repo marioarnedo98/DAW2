f34016badbb1222915e02a148ce490b1dd3196ab
Mario <marioarnedo1@gmail.com>
2019-05-05 17:37:18 +0200

    0505

--- Desarrollo Web Entorno Cliente/3Ev/Teoria/E3.4 Success and Error Handling/Exercise2/a72.js
+++ Desarrollo Web Entorno Cliente/3Ev/Teoria/E3.4 Success and Error Handling/Exercise2/a72.js
@@ -1,23 +1,24 @@
-$(function(){
-    $('confirmation').on('click', 'button',function(){
-        $.ajax('confirmation.html',{
-            timeout: 3000,
-            success: function(response){
-                $('ticket').html(response).slideDown();
-            },
-            //manejar errores en AJAX:
-            error: function(xhr){
-                $('ticket').html(xhr.responseText).slideDown();
-            }
-        });
+$(function() { 
+    $('.confirmation').on('click', 'button', function() {
+      $.ajax('confirmation.html', {
+        timeout: 3000,
+        success: function(response) {
+          $('.ticket').html(response).slideDown();
+        },
+        error: function(request, errorType, errorMessage) {
+          alert('Error: ' + errorType + ' with message: ' + errorMessage);
+        },
+        beforeSend: function() {
+          $('.confirmation').addClass('is-loading');
+        },
+        complete: function() {
+          $('.confirmation').removeClass('is-loading');
+        }
+      });
     });
-    $('ticket').on('click', '.view-boarding-pass', function(e){
-        e.preventDefault();
-        $('.view-boarding-pass').hide();
-        $('.boarding-pass').show();
+    $('.ticket').on('click', '.view-boarding-pass', function(e) {
+      e.preventDefault();
+      $('.view-boarding-pass').hide();
+      $('.boarding-pass').show();
     })
-})
-/*
-La opción xhr le permite definir su propia devolución de llamada
- para crear el objeto XMLHttpRequest que se utilizará en el ajax ()
-*/
+  });
